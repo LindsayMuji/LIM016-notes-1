0699505ee5698c6f9d63b88c6bb3d55b96e6cc4d
LindsayMuji <lmelissa.mp@gmail.com>
2022-04-04 11:53:06 -0500

    inicio de test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+beforeEach (() => {
   render(<App />);
-  const linkElement = screen.getByText(/welcome to/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+test('renders to the welcome', () => {
+  const welcomeText = screen.getByText(/welcome to/i);
+  expect(welcomeText).toBeInTheDocument();
+});
+
+test('renders Simple Notes Titlle', () => {
+  const tittle = screen.getByText(/Simple Notes/i);
+  expect(tittle).toBeInTheDocument();
+});
+
+test('renders input placeholder', () => {
+  const emailPlac = screen.getByPlaceholderText(/enter your email/i);
+  expect(emailPlac).toBeInTheDocument();
+});
+
+
+/* test('renders form elements', () => {
+  const btnEl = screen.getByText('Button',{ name: /Log In/i});
+  expect(btnEl).toBeInTheDocument();
+}); */
